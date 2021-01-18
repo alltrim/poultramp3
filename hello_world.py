1243e66c61db1ec47b9c3a2f62212f09f71155e6
alltrim <linnik@live.com>
2021-01-18 17:01:38 +0200

    loop

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,9 @@
 import time
 
 def main():
-    print("Hello, world at ", time.strftime('%Y.%m.%d %H:%M:%S'))
+    while true:
+        print("Hello, world at ", time.strftime('%Y.%m.%d %H:%M:%S'))
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
