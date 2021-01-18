072c0f57f800eebb0b6192c12988d4ff019b9566
alltrim <linnik@live.com>
2021-01-18 17:09:46 +0200

    0.1

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ import time
 def main():
     while True:
         print("Hello, world at ", time.strftime('%Y.%m.%d %H:%M:%S'))
-        time.sleep(5)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
