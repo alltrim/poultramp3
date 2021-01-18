359325d92b188f33a9b17d7a4b281d58d3102db3
alltrim <linnik@live.com>
2021-01-18 14:05:05 +0200

    new

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,8 @@
 import time
 
 def main():
-    print("Hello? world at ", time.time())
+    print("Hello, world at ", time.strftime('%Y.%m.%d %H:%M:%S'))
 
 if __name__ == "__main__":
     main()
 
-    
