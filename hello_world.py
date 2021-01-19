4cf759f34ee957fb30327c94373d083c5d757a21
alltrim <linnik@live.com>
2021-01-19 11:50:57 +0200

    all done

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,15 @@
 import time
 
+def log(*args, **kwargs):
+    with open("hello.log", "a") as file:
+        file.writelines(args)
+        file.write("\r\n")
+
+
 def main():
     while True:
-        print("Hello, world at ", time.strftime('%Y.%m.%d %H:%M:%S'))
-        time.sleep(0.1)
+        log("Hello, world at ", time.strftime('%Y.%m.%d %H:%M:%S'))
+        time.sleep(10)
 
 if __name__ == "__main__":
     main()
