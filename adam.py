f2d177be037fb7f2bdb50ee671f452f825174143
alltrim <linnik@live.com>
2021-01-25 22:55:43 +0200

    #10

--- adam.py
+++ adam.py
@@ -1,15 +1,17 @@
 import serial
 import time
 
-DEVSERIAL = "/dev/tty.usbserial-1410"
-#DEVSERIAL = "/dev/ttyS1"
+#DEVSERIAL = "/dev/tty.usbserial-1410"
+DEVSERIAL = "/dev/ttyS1"
 
 ser = serial.Serial(DEVSERIAL, baudrate=9600, timeout=1)
-ser.timeout
+ser.flush()
 
 while True:
-    ser.write(b'$016\x0D')
+    mes = bytes("$016\r", encoding="ascii")
+    ser.write(mes)
+    print(mes)
 
-    resp = ser.read_until(expected=serial.CR)
+    resp = ser.read_until(serial.CR)
     print(resp)
-    time.sleep(1)
+    time.sleep(1)  
